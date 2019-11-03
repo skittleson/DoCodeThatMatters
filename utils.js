4b760bb751ef5bb75a049663461106ffeb45fb57
Spencer Kittleson <spencerkittleson@hotmail.com>
2019-11-03 10:42:34 -0800

    sitemap updates

--- utils.js
+++ utils.js
@@ -80,7 +80,7 @@ module.exports.buildSiteFromJson = function buildSiteFromJson(json, src, dest) {
   );
 
   // Create site map
-  let siteMap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="https://www.schema.org/schemas/sitemap/0.9">`;
+  let siteMap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">`;
   store.pages
     .filter(page => page.file !== 'admin.hbs')
     .filter(page => page.file !== 'offline.hbs')
