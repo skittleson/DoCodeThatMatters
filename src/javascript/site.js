52715afb94ba83693e06fa991faa8c6e85120e6a
Spencer <spencerkittleson@hotmail.com>
2022-03-18 14:23:41 -0700

    contact us

--- src/javascript/site.js
+++ src/javascript/site.js
@@ -7,13 +7,34 @@ if ("serviceWorker" in navigator) {
     // Register the service worker
     navigator.serviceWorker
       .register("/sw.js", {
-        scope: "./"
+        scope: "./",
       })
-      .then(function(reg) {
+      .then(function (reg) {
         console.log(
           "[PWA Builder] Service worker has been registered for scope: " +
             reg.scope
         );
       });
   }
+}
+
+async function fetchContactRelay(request, endpoint) {
+  try {
+    const fetchTokenResponse = await fetch(endpoint);
+    const tokenResponse = await fetchTokenResponse.json();
+    const token = tokenResponse.token;
+    console.log(token);
+  } catch (error) {
+    alert("Unable to send message");
+  }
+
+  // const response = await fetch(endpoint, {
+  //   method: "POST",
+  //   // mode: "no-cors",
+  //   headers: {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(request),
+  // });
 }
